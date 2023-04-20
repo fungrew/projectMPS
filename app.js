2b2f6cec038cc51d676ddf2ec06b312460546ee6
fungrew <39476039+fungrew@users.noreply.github.com>
2023-04-20 12:35:26 +0700

    add or file

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const os = require("os")
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+app.use(morgan('combined'))
+
+app.get("/",(req,res)=>{
+
+    res.send('Hello Doiiii');
+
+})
+
+app.listen(port,()=>{
+   debug("lisening on port  "+chalk.green(":"+port));
+})
