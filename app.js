3be1f33441600ab627d156e11b2a9ee02068dfbd
fungrew <39476039+fungrew@users.noreply.github.com>
2023-04-20 13:56:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 const app = express();
 const port = 3000;
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public")));
 
 app.get("/",(req,res)=>{
