26da2f603e4a6b8e6f909312ddeef3d707eee282
fungrew <39476039+fungrew@users.noreply.github.com>
2023-04-20 12:54:03 +0700

    1

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const os = require("os")
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.get("/",(req,res)=>{
 
