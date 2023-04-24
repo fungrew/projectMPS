6d44fd0fb188b42ea877efceed9530f759d93685
fungrew <39476039+fungrew@users.noreply.github.com>
2023-04-24 10:22:29 +0700

    1

--- app.js
+++ app.js
@@ -4,18 +4,26 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const { debuglog } = require('util');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public")));
+app.set("views","./src/views");
+app.set("view engine","ejs")
+
 
 app.get("/",(req,res)=>{
 
-    res.send('Hello Jarun');
+    res.render('index',{username:'KRUDOIOOOO',costomers:["DOI1","DOI2","DOI3"]});
+
 
 })
 
-app.listen(port,()=>{
-   debug("lisening on port  "+chalk.green(":"+port));
+app.listen(PORT,()=>{
+    debuglog("Lisening on"+chalk.green(PORT))
+   debug("lisening on"+chalk.red(PORT))
+   console.log("Listrning to"+chalk.red(PORT))
 })
+ 
